718efe58d2831fa875fae8a337c41c88bd55079f
Hanna Evensen <hannaevensen2405@gmail.com>
2018-10-02 16:28:17 +0200

    #1 add goal page

---
+++ src/components/goal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+
+
+class Goal extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Goal</Text>
+      </View>
+    );
+  }
+}
+
+export default Goal;
