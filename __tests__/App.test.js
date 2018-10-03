d837c41345910de612cb8fc3df2e71eafe32295a
Tania Holst <holst.tania@gmail.com>
2018-10-03 18:31:41 +0200

    #11 setup for tests

---
+++ __tests__/App.test.js
@@ -0,0 +1,26 @@
+import 'react-native';
+import React from 'react';
+
+//import { Goal } from '../components/goal.js';
+import { hello } from '../App.js';
+
+console.log(hello());
+
+/* 
+describe('Testing goal component', () => {
+it('renders without crashing', () => {
+  const wrapper = shallow(<App />)
+  expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
+
+describe('Testing goal component', () => {
+    //expect home.js to be true
+    //click goal
+    //expect goal.js to be true
+  it('renders as expected', () => {
+
+  });
+});
+
+ */
