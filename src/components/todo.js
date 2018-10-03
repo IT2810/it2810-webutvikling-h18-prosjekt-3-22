896d6a7ddded9fdaacc2c0e090c5dfc1c605a2a0
Hanna Evensen <hannaevensen2405@gmail.com>
2018-10-03 17:52:18 +0200

    #7 add basic structure and stylesheet for todo

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,17 +1,104 @@
-import React, { Component } from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
+
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  ScrollView,
+  View,
+  TouchableOpacity
+} from "react-native";
 
 
 
 class Todo extends Component {
-  render() {
+  render(){
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screenkk</Text>
-      </View>
+        <View style={styles.container}>
+          <View style={styles.header}>
+              <Text style={styles.headerText}>- Noter -</Text>
+          </View>
+
+          <ScrollView style={styles.scrollContainer}>
+
+          </ScrollView>
+
+
+          <TextInput
+              style={styles.textInput}
+              placeholder=">note"
+              placeholderTextColor="white"
+              underlineColorAndroid="transparent">
+          </TextInput>
+
+
+        </View>
     );
   }
 }
 
-export default Todo;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  header: {
+    backgroundColor: '#E91E63',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 10,
+    borderBottomColor: "#ddd",
+  },
+
+  headerText: {
+    color: 'white',
+    fontSize: 18,
+    padding: 26,
+  },
+
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100,
+  },
+
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+  },
+
+  textInput: {
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    backgroundColor: "#252525",
+    borderTopWidth: 2,
+    borderTopColor: "#ededed",
+  },
+
+  addButton: {
+    position: 'absolute',
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: "#E91E63",
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 8,
+  },
+
+  addButtonText: {
+    color: "#fff",
+    fontSize: 25,
+  },
+
+
+});
+
+export default Todo
