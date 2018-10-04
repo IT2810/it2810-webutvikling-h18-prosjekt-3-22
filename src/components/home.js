40e8f5146565070d807304955994fd67b73b3231
Tania Holst <holst.tania@gmail.com>
2018-10-04 10:26:17 +0200

    #11 added pageid to home and button ids to todo and goal

--- src/components/home.js
+++ src/components/home.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 
-
-
 class Home extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View id='homePage' style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
         <View style={{flex:1 , marginTop:100, width:200}} >
             <Text>Home Screen</Text>
@@ -15,6 +13,7 @@ class Home extends Component {
             title={`To do`}
             onPress={() => this.props.navigation.navigate('Todo')}
             icon={{name: 'check-square-o', type: 'font-awesome', size: 60}}
+            id={'toDoBtn'}
             />
         </View>
         <View style={{flex:1 , marginBottom:100, width:200}} >
@@ -22,12 +21,10 @@ class Home extends Component {
             onPress={() => this.props.navigation.navigate('Goal')}
             icon={{name: 'flag', type: 'font-awesome', size: 60}}
             title={`Goal`}
+            id={'goalBtn'}
             />
         </View>
       </View>
-
-
-
     );
   }
 }
