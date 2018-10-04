dc623493ce4ab4738a594cdf33725290ae711d79
Tania Holst <holst.tania@gmail.com>
2018-10-04 10:25:35 +0200

    #11 testing now works fine, the right packages are included

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,26 +1,44 @@
 import 'react-native';
 import React from 'react';
+//import { shallow, mount, render } from 'enzyme';
+import renderer from 'react-test-renderer'
 
-//import { Goal } from '../components/goal.js';
-import { hello } from '../App.js';
+import Home from '../src/components/home.js';
+import Todo from '../src/components/todo.js';
+import Goal from '../src/components/goal.js';
 
-console.log(hello());
+const sum = require('../src/components/testen');
 
-/* 
-describe('Testing goal component', () => {
+//test to see if tests are working
+/*describe('Sum test', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});*/
+
+//creates snapshots from home-component
+//need to write 'npm test -- -u' to update to new snapshot
+test ('Home snapshot', () => {
+  const snap = renderer.create(
+    <Home />
+  ).toJSON();
+expect(snap).toMatchSnapshot();
+});
+
+/* describe('Home page test', () => {
+    beforeEach(() => {
+      //check that you actually are on the home page
+      initializeCityDatabase();
+      expect()
+    });
+}); */
+
+
+/*
+describe('Test rendering', () => {
 it('renders without crashing', () => {
   const wrapper = shallow(<App />)
   expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
-
-describe('Testing goal component', () => {
-    //expect home.js to be true
-    //click goal
-    //expect goal.js to be true
-  it('renders as expected', () => {
-
-  });
-});
-
- */
+}); */
