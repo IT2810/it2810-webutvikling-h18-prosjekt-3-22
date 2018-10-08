a8eeeea8782b3b2891d3e38c30f5f4c42d157cdf
Tania Holst <holst.tania@gmail.com>
2018-10-08 20:07:27 +0200

    #11 fix in home test

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -3,15 +3,15 @@ import React from 'react';
 import renderer from 'react-test-renderer'
 import Home from '../src/components/home.js';
 
-let findElement = function (tree, element) {
+let findButton = function (tree, element) {
   let result = undefined;
   let todoBtn = tree.children[0].children[1].children[0].props.testID;
   let goalBtn = tree.children[1].children[0].children[0].props.testID;
-  for (node in tree.children) {
+  //for (node in tree.children) {
     if (todoBtn == element || goalBtn == element) {
       result = true;
     }
-  }
+  //}
   return result;
 }
 
@@ -25,10 +25,9 @@ describe('Home-page test', () => {
   });
 //tests if the buttons are there
   test('To do-button', () => {
-    expect(findElement(tree, 'toDoBtn')).toBe(true);
+    expect(findButton(tree, 'toDoBtn')).toBe(true);
   });
   test('Goal-button', () => {
-    expect(findElement(tree, 'goalBtn')).toBe(true);
-
+    expect(findButton(tree, 'goalBtn')).toBe(true);
   });
 });
