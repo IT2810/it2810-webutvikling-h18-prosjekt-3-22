dc18c92c5bd56138793632167b9dff666495884a
Tania Holst <holst.tania@gmail.com>
2018-10-08 20:08:15 +0200

    #11 Added tests on goal page

--- __tests__/Goal.test.js
+++ __tests__/Goal.test.js
@@ -3,10 +3,45 @@ import React from 'react';
 import renderer from 'react-test-renderer'
 import Goal from '../src/components/goal.js';
 
+let findSteps = function (tree, element) {
+  let result = undefined;
+  //let pageStepGoal = tree.children[1].children[3];
+  let steps = tree.children[1];
+  for (node in steps.children) {
+    if (steps.children[node] == element) {
+      result = true;
+    }
+  }
+  return result;
+}
+
+let findInput = function (tree, element) {
+  let result = undefined;
+  for (node in tree.children) {
+    if (tree.children[node].props.testID == element) {
+      result = true;
+    }
+  }
+  return result;
+}
+
 //creates snapshots from goal-component
-test('Take snapshot of Goal', () => {
-  const snap = renderer.create( <
+describe('Goal-page test', () => {
+  const tree = renderer.create( <
     Goal / >
   ).toJSON();
-  expect(snap).toMatchSnapshot();
+  test('Take snapshot of goal-component', () => {
+    expect(tree).toMatchSnapshot();
+  });
+  test('Verify that stepgoal is 10000', () => {
+    let stepGoal = 10000;
+    expect(findSteps(tree, stepGoal)).toBe(true);
+  });
+  test('Verify that steps taken today is 0', () => {
+    let stepsToday = 0;
+    expect(findSteps(tree, stepsToday)).toBe(true);
+  });
+  test('Check if inputbox is available', () => {
+    expect(findInput(tree, 'goalInput')).toBe(true);
+  });
 });
