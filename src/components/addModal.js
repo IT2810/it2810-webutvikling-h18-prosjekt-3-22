f9a83e20d73d3f6a6b8bf1ee5e6825529d438fc4
hanna <hannaevensen2405@gmail.com>
2018-10-15 14:30:14 +0200

    #9 add textfield for new appointment and date in popup modal

--- src/components/addModal.js
+++ src/components/addModal.js
@@ -12,25 +12,99 @@ import flatListData from '../data/flatListData';
 
 var screen = Dimensions.get('window');
 export default class AddModal extends Component{
-  constructor(props){
-    super(props);
+  constructor(props) {
+        super(props);
+        this.state = {
+            newFoodName: '',
+            newDate: ''
+        };
+    }
+
+  showAddModal = () => {
+    this.refs.myModal.open();
   }
 
   render(){
     return(
-      <Modal style={{
-        justifyContent: 'center',
-        borderRadius: Platform.OS === "ios" ? 30 : 0,
-        width: screen.width - 80,
-        height: 200
-      }}>
-      position='center'
-      backdrop={true}
-      onclosed={() => {
-        alert("Modal closed")
-      }}
-      <Text>New food info</Text>
-    </Modal>
+      <Modal
+          ref={"myModal"}
+                style={{
+                    justifyContent: 'center',
+                    borderRadius: Platform.OS === 'ios' ? 30 : 0,
+                    shadowRadius: 10,
+                    width: screen.width - 80,
+                    height: 280
+                }}
+                position='center'
+                backdrop={true}
+                onClosed={() => {
+                    // alert("Modal closed");
+                }}
+            >
+                <Text style={{
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    marginTop: 40
+                }}>New appointment</Text>
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderBottomColor: 'gray',
+                        marginLeft: 30,
+                        marginRight: 30,
+                        marginTop: 20,
+                        marginBottom: 10,
+                        borderBottomWidth: 1
+                    }}
+                    onChangeText={(text) => this.setState({ newFoodName: text })}
+                    placeholder="Enter your appointment"
+                    value={this.state.newFoodName}
+                />
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderBottomColor: 'gray',
+                        marginLeft: 30,
+                        marginRight: 30,
+                        marginTop: 10,
+                        marginBottom: 20,
+                        borderBottomWidth: 1
+                    }}
+
+                    onChangeText={(text) => this.setState({ newDate: text })}
+                    placeholder="Enter date"
+                    value={this.state.newDate}
+                />
+                <Button
+                    style={{ fontSize: 18, color: 'white' }}
+                    containerStyle={{
+                        padding: 8,
+                        marginLeft: 70,
+                        marginRight: 70,
+                        height: 40,
+                        borderRadius: 6,
+                        backgroundColor: 'mediumseagreen'
+                    }}
+                    onPress={() => {
+                         if (this.state.newFoodName.length == 0 || this.state.newDate.length == 0) {
+                            alert("You must enter food's name and description");
+                            return;
+                        }
+                        const newKey = this.generateKey(24);
+                        const newFood = {
+                            key: newKey,
+                            name: this.state.newFoodName,
+                            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg",
+                            foodDescription: this.state.newDate
+                        };
+                        flatListData.push(newFood);
+                        this.props.parentFlatList.refreshFlatList(newKey);
+                        this.refs.myModal.close();
+                    }}>
+                    Save
+                </Button>
+            </Modal>
     )
   }
 }
--- src/components/appointments.js
+++ src/components/appointments.js
@@ -3,15 +3,16 @@
 // import App from './App';
 
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, FlatList, StyleSheet, Text, View, Image, Alert, Platform, TouchableHighlight } from 'react-native';
 import flatListData from '../data/flatListData';
+import AddModal from './addModal';
 
 class FlatListItem extends Component {
     render() {
         return (
           <View style={{
-                flex: 1,
-                backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen': 'tomato'
+                flex: 1
+
           }}>
               <Text style={styles.flatListItem}>{this.props.item.name}</Text>
               <Text style={styles.flatListItem}>{this.props.item.foodDescription}</Text>
@@ -28,21 +29,46 @@ const styles = StyleSheet.create({
 });
 
 export default class Appointments extends Component {
+  constructor(props){
+    super(props);
+    this._onPressAdd = this._onPressAdd.bind(this);
+  }
+  _onPressAdd(){
+    this.refs.addModal.showAddModal();
+  }
     render() {
       return (
         <View style={{flex: 1, marginTop: 22}}>
+        <View style={{
+         backgroundColor: 'tomato',
+         flexDirection: 'row',
+         justifyContent:'flex-end',
+         alignItems: 'center',
+         height: 64}}>
+         <TouchableHighlight
+             style={{marginRight: 10}}
+             underlayColor='tomato'
+             onPress={this._onPressAdd}
+             >
+             <Text>ggg</Text>
+     </TouchableHighlight>
+     </View>
             <FlatList
+                ref={"flatList"}
                 data={flatListData}
                 renderItem={({item, index})=>{
                     //console.log(`Item = ${JSON.stringify(item)}, index = ${index}`);
                     return (
-                    <FlatListItem item={item} index={index}>
+                    <FlatListItem item={item} index={index} parentFlatList={this}>
 
                     </FlatListItem>);
                 }}
                 >
 
             </FlatList>
+            <AddModal ref={'addModal'} parentFlatList={this} >
+
+            </AddModal>
         </View>
       );
     }
