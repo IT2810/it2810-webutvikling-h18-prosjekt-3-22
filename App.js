d8fd6119377baea5615edc2724f92b922cf92990
heddah <heddaholmen@dhcp-10-24-194-130.wlan.ntnu.no>
2018-10-16 13:32:08 +0200

    #8 static contacts

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import TodoScreen from './src/components/todo.js'
 import HomeScreen from './src/components/home.js'
 import GoalScreen from './src/components/goal.js';
 import ContactHomeScreen from './src/components/contactHome.js';
-import ContactDetailScreen from './src/components/contactDetail.js';
 
 import {createStackNavigator} from 'react-navigation';
 
@@ -15,8 +14,7 @@ const RootStack = createStackNavigator(
     Home: HomeScreen,
     Todo: TodoScreen,
     Goal: GoalScreen,
-    ContactHome: ContactHomeScreen,
-    ContactDetail: ContactDetailScreen
+    ContactHome: ContactHomeScreen
   },
   {
     initialRouteName: 'Home',
--- src/components/contactHome.js
+++ src/components/contactHome.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, Modal, TouchableHighlight, Alert} from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Modal, AsyncStorage, Alert, FlatList} from 'react-native';
 
 class ContactHome extends Component {
 
     constructor (){
         super();
         this.state = {
-            contactArray: []
+            contactArray: [["Ola", "123", "@hhi"], ["Kari", "89398", "dj@jkj.no"], ["Jens", "983", "ss@fr.no"]],
+            modalVisible: false,
+            refresh: false
         }
     }
-
-    state = {
-        modalVisible: false,
-    };
 
     setModalVisible(visible) {
         this.setState({modalVisible: visible});
     }
 
-    newContact = () => {
-        u
+    newContact = (name, number, mail) => {
+        let contact = [
+            "",
+            "",
+            ""
+        ];
+
+        contact[0] = name;
+        contact[1] = number;
+        contact[3] = mail;
+
+        this.state.contactArray.concat({person: contact});
+        this.state.refresh = true
+        //Tasks.save(this.state.contactArray);
     }
 
+    viewDetails=(item)=>{
+        let name = item[0];
+        let number = item[1];
+        let mail = item[2];
+
+        Alert.alert(name, ('Number: ' + number + '\n' + 'Mail: '+ mail));
+    };
+
     render () {
+        let newName = "";
+        let newNumber = "";
+        let newMail = "";
+
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
@@ -40,7 +62,20 @@ class ContactHome extends Component {
                         <Text style={styles.headerText}> Add new contact </Text>
                     </View>
 
-
+                    <View style={styles.inputWrapper}>
+                        <View style={styles.input}>
+                            <Text style={styles.inputText}>Name:</Text>
+                            <TextInput placeholder={"Ola Nordmann"} onChangeText={input => newName = input}/>
+                        </View>
+                        <View style={styles.input}>
+                            <Text style={styles.inputText}>Number:</Text>
+                            <TextInput placeholder={"123456789"} onChangeText={input => newNumber = input}/>
+                        </View>
+                        <View style={styles.input}>
+                            <Text style={styles.inputText}>Mail:</Text>
+                            <TextInput placeholder={"ola@gmail.com"} onChangeText={input => newMail = input}/>
+                        </View>
+                    </View>
 
                     <View style={styles.btnWrapper}>
                         <Button title={"✖Close"} onPress={() => {
@@ -48,6 +83,7 @@ class ContactHome extends Component {
                         }}>
                         </Button>
                         <Button title={"✔Save"} onPress={() => {
+                            this.newContact(newName, newNumber, newMail);
                             this.setModalVisible(!this.state.modalVisible);
                         }}>
                         </Button>
@@ -56,17 +92,40 @@ class ContactHome extends Component {
                 </Modal>
             {/*End modal*/}
 
-            <Button title={"Add contact"}
-                onPress={() => {
-                    this.setModalVisible(true);
-                }}>
-            </Button>
+                <FlatList
+                data={this.state.contactArray}
+                extraData={this.state.refresh}
+                renderItem={({item, key}) => (
+                    <View style={styles.textBox}>
+                    <Text key={key} style={styles.textStyle} onPress={ this.viewDetails.bind(this, item) }> { item[0] } </Text>
+                    </View>
+                )}
+                />
+
+                <View style={styles.addBtn}>
+                    <Button title={"Add contact"}
+                        onPress={() => {
+                            this.setModalVisible(true);
+                        }}>
+                    </Button>
+                </View>
             </View>
         </View>
         );
     }
 
 }
+
+let Tasks = {
+    saveContacts(contactArray) {
+        AsyncStorage.setItem("CONTACTS", this.state.contactArray);
+    },
+
+    loadContacts() {
+        return AsyncStorage.getItem("CONTACTS", contactArray);
+        this.state.contactArray = contactArray
+    }
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -95,6 +154,43 @@ const styles = StyleSheet.create({
 
     content: {
         marginTop: 80
+    },
+
+    inputWrapper: {
+        margin: 20
+    },
+
+    input: {
+        flexDirection: 'row',
+        marginBottom: 10,
+        borderWidth: 1,
+        padding: 15
+    },
+
+    inputText: {
+        marginRight: 5
+    },
+
+    textBox: {
+        marginBottom: 10,
+        padding: 5,
+        backgroundColor: "skyblue",
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 10,
+        marginLeft: 10
+    },
+
+    textStyle: {
+        marginBottom: 10,
+        marginTop: 10,
+        marginLeft: 20,
+        width: '100%',
+        fontWeight: '600'
+    },
+
+    addBtn: {
+
     }
 });
 
