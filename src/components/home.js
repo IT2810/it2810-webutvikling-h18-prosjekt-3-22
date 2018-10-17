4f26ed2a0b989838b88a76672dad99d9c9bab804
Tania Holst <holst.tania@gmail.com>
2018-10-17 16:01:13 +0200

    #1 added changes to home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 
 class Home extends Component {
   render() {
     return (
-        <View style={styles.container}>
+      <View style={styles.container}>
           <Text style={styles.title}>
             P.I.M.M.
           </Text>
-          <View style={styles.todo} >
-              <Button
-              title={`To do`}
-              testID={'toDoBtn'}
-              onPress={() => this.props.navigation.navigate('Todo')}
-              icon={{name: 'check-square-o', type: 'font-awesome', size: 60}}
-              />
-          </View>
-          <View style={styles.goal} >
-              <Button
-              title={`Goal`}
-              testID={'goalBtn'}
-              onPress={() => this.props.navigation.navigate('Goal')}
-              icon={{name: 'flag', type: 'font-awesome', size: 60}}
-              />
-          </View>
+          <View style={styles.todo}>
+            <Button
+            onPress={() => this.props.navigation.navigate('Todo')}
+            title={`✅ To do`}
+            testID={'toDoBtn'}
+            />
+        </View>
+        <View style={styles.goal} >
+            <Button
+            onPress={() => this.props.navigation.navigate('Goal')}
+            title={`🏆 Goal`}
+            testID={'goalBtn'}
+            />
+        </View>
+        <View style={styles.contacts} >
+            <Button
+              onPress={() => this.props.navigation.navigate('ContactHome')}
+              title={`👥 Contacts`}
+              testID= {'contactsBtn'}
+            />
+        </View>
       </View>
     );
   }
@@ -37,6 +43,7 @@ const styles = StyleSheet.create({
     //backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    //margin: 100,
   },
   title: {
     textAlign: 'center',
@@ -46,15 +53,19 @@ const styles = StyleSheet.create({
   todo: {
     flex: 1,
     marginTop: 50,
-    width: 200,
+    width: 300,
+    //height: 400,
   },
   goal: {
     flex: 1,
-    marginBottom: 100,
-    width: 200,
+    //marginBottom: 100,
+    width: 300,
   },
-
-
+  contacts: {
+    flex: 1,
+    marginBottom: 100,
+    width: 300,
+  }
 
 /** This doesn't work??
   button: {
