45572be362c56e512e14ba6dbcab89bdaca22ec9
Tania Holst <holst.tania@gmail.com>
2018-10-19 07:43:57 +0200

    small changes in Goal.test.js

--- __tests__/Goal.test.js
+++ __tests__/Goal.test.js
@@ -3,18 +3,6 @@ import React from 'react';
 import renderer from 'react-test-renderer'
 import Goal from '../src/components/goal.js';
 
-/* Finds the elements where it says how many steps are taken that day, and your goal */
-let findSteps = function (tree, element) {
-  let result = undefined;
-  let steps = tree.children[2].children[0];
-  for (node in steps.children) {
-    if (steps.children[node] == element) {
-      result = true;
-    }
-  }
-  return result;
-}
-
 /* Finds the inputbox */
 let findInput = function (tree, element) {
   let result = undefined;
@@ -27,6 +15,7 @@ let findInput = function (tree, element) {
 }
 
 describe('Goal-page test', () => {
+  const steps = tree.children[2].children[0];
   const tree = renderer.create( <
     Goal / >
   ).toJSON();
@@ -36,13 +25,13 @@ describe('Goal-page test', () => {
   });
   /* Test to verify that the stepgoal is 10 000 before you set your own goal*/
   test('Verify that stepgoal is 10000', () => {
-    let stepGoal = 10000;
-    expect(findSteps(tree, stepGoal)).toBe(true);
+    let stepGoal = steps.children[2];
+    expect(stepGoal).toEqual('10000');
   });
   /* Test to verify that the steps taken today starts with 0 */
   test('Verify that steps taken today is 0', () => {
-    let stepsToday = 0;
-    expect(findSteps(tree, stepsToday)).toBe(true);
+    const stepsToday = steps.children[0];
+    expect(stepsToday).toEqual('0');
   });
   /* Test to verify that the inputbox where you input goal is available */
   test('Check if inputbox is available', () => {
